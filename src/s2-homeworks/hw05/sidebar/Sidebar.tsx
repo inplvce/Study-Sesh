d485b270c4891c6f2011cf49ad988d39f7e5de8e
ukaniushenka <inplvce@gmail.com>
2024-09-10 07:40:56 +0300

    5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -10,7 +10,9 @@ type PropsType = {
 }
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
-    const sidebarClass = `${s.sidebar} ${open ? ' ' + s.open : ''}`
+    const sidebarClass = `${s.sidebar} ${open ? s.open : ''}`;
+
+
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -30,7 +32,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={s.active}
+                        className={({ isActive }) => (isActive ? s.active : '')}
                         // className={...} // делает студент
                     >
                         Pre-junior
@@ -39,7 +41,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={s.active}
+                        className={({ isActive }) => (isActive ? s.active : '')}
                         // className={...} // делает студент
                     >
                         Junior
@@ -48,7 +50,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={s.active}
+                        className={({ isActive }) => (isActive ? s.active : '')}
                         // className={...} // делает студент
                     >
                         Junior Plus
